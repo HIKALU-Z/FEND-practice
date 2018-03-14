e8991aeb9ac74742850a0d8643e82b315a33d2e9
HIKALU <play3a@126.com>
2018-03-14 11:12:46 +0800

    add new practice

--- practice/002-html-tag/js/main.js
+++ practice/002-html-tag/js/main.js
@@ -1,16 +1,27 @@
 ;(function(){
-    var Cat = function(){
-        this.counter = 0
+    var Cat = function(id){
+        this.counter = 0;
+        this.catID = 'cat-'+id;
+        this.counterID = 'counter-'+id;
     }
-    Cat.prototype.add = function(){
-        console.log('1')
+    Cat.prototype.add = function(id){
+        this.counter++;
+        document.getElementById(id).innerText = this.counter;
     }
-    var counter1  = document.getElementById('click-counter-1');
-    var counter2  = document.getElementById('click-counter-2');
-    counter1.addEventListener('click', function(){
-        console.log('1')
-        //the element has been clicked... do stuff here
-    }, false);
-    counter1.innerText = 1;
-    counter2.innerText = 2;
+    Cat.prototype.onClicked = function(){
+        var cat = document.getElementById(this.catID);
+        id = this.catID;
+        
+        cat.addEventListener('click',function(){
+            cat1.add()
+        })
+    }
+    var cat1 = new Cat(01);
+    var cat2 = new Cat(02);
+    cat1.onClicked()
+    // var counter2  = document.getElementById('cat-2');
+    // counter1.addEventListener('click', function(){
+    //     cat1.add('counter-1');
+    //     //the element has been clicked... do stuff here
+    // }, false);
 })();
