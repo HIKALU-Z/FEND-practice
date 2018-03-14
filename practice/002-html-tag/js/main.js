981e64716686a48e0290f7e38ea1ad53d2437ffe
HIKALU <play3a@126.com>
2018-03-14 18:15:12 +0800

    完成知乎页面结构

--- practice/002-html-tag/js/main.js
+++ practice/002-html-tag/js/main.js
@@ -19,9 +19,4 @@
     var cat1 = new Cat(01);
     var cat2 = new Cat(02);
     cat1.onClicked()
-    // var counter2  = document.getElementById('cat-2');
-    // counter1.addEventListener('click', function(){
-    //     cat1.add('counter-1');
-    //     //the element has been clicked... do stuff here
-    // }, false);
 })();
