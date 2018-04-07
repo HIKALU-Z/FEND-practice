c047e4de19c3bb2b77ffbefeac628143fd42c807
HIKALU <play3a@126.com>
2018-04-07 16:49:46 +0800

    feat: add blog project
    use gulp and flex-box to write a blog

---
+++ project/003-blog/gulpfile.js
@@ -0,0 +1,47 @@
+'use strict'
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin = require('gulp-imagemin')
+const pngquant = require('imagemin-pngquant')
+
+
+function handleError(err) {
+  console.log(err.toString())
+  this.emit('end')
+}
+
+gulp.task('imagemin', function() {
+  return gulp
+    .src('src/img/*')
+    .pipe(
+      imagemin({
+        use: [pngquant()]
+      })
+    )
+    .pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('sass', function() {
+  gulp
+    .src('src/css/main.scss')
+    .pipe(sass())
+    .on('error', handleError)
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions']
+      })
+    )
+    .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('copy', function() {
+  gulp.src('src/*.html').pipe(gulp.dest('dist/'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch(['src/*.html'], ['copy'])
+  gulp.watch(['src/css/*.scss'], ['sass'])
+})
+
+gulp.task('default', ['sass', 'copy', 'watch'])
