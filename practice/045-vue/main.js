6850e9a828c851e4b16f2ca6ceef62be006f72bd
HIKALU <play3a@126.com>
2018-06-14 20:32:14 +0800

    add new vue pratice

--- practice/045-vue/main.js
+++ practice/045-vue/main.js
@@ -1,27 +1,50 @@
 new Vue({
   el: '#root',
   data: {
-    memoList: [
-      {
-        title: 'title A',
-        content: 'AAAAA'
-      },
-      {
-        title: 'title B',
-        content: 'BBBBB'
-      },
-      {
-        title: 'title C',
-        content: 'CCCCCC'
-      }
-    ],
+    memoList: [],
     current: null
   },
   methods: {
     create() {
-      this.current = {};
+      this.current = {
+        title: '新增的一条备忘'
+      };
       this.memoList.push(this.current);
     },
-    onMemoClick() {}
-  }
+    update() {
+
+    },
+    remove(index) {
+      this.memoList.splice(index, 1)
+    },
+    sync_to(memoList) {
+      localStorage.setItem('memoList', JSON.stringify(memoList));
+    },
+    sync_from() {
+      this.memoList = JSON.parse(localStorage.getItem('memoList')) || [{
+          title: 'title A',
+          content: 'aaaa',
+        },
+        {
+          title: 'title B',
+          content: 'bbbb',
+        },
+        {
+          title: 'title C',
+          content: 'cccc',
+        },
+      ];
+    }
+  },
+  watch: {
+    memoList: {
+      deep: true,
+      handler: function (n) {
+        this.sync_to(n);
+      },
+    },
+  },
+  created: function () {
+    this.sync_from();
+  },
 });
