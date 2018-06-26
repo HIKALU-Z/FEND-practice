d90e9c69457ecefa99c3adb997065f9bbb01a5fe
HIKALU <play3a@126.com>
2018-06-26 09:42:52 +0800

    add new practice

--- project/008-yyc/src/App.vue
+++ project/008-yyc/src/App.vue
@@ -8,15 +8,14 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Home from './page/Home.vue'
-
+import Home from './page/Home.vue';
 export default {
   name: 'app',
   components: {
     // HelloWorld,
     Home
   }
-}
+};
 </script>
 
 <style>
@@ -24,8 +23,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f2f2f2;
+  /* margin-top: 60px; */
 }
 </style>
--- project/008-yyc/src/main.js
+++ project/008-yyc/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import 'minireset.css';
+import '../node_modules/bulma/css/bulma.css';
+import './main.css';
+// import 'bulma'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
