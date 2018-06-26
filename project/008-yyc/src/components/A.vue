ed9745c5b96da430c08b53028ddc28352ebb82f1
HIKALU <play3a@126.com>
2018-06-26 10:28:31 +0800

    add router

---
+++ project/008-yyc/src/components/A.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>hell</h1>
+</template>
--- project/008-yyc/src/main.js
+++ project/008-yyc/src/main.js
@@ -4,9 +4,26 @@ import './main.css';
 // import 'bulma'
 import Vue from 'vue';
 import App from './App.vue';
+import Router from 'vue-router';
+import A from './components/A.vue';
+import B from './components/B.vue';
 
-Vue.config.productionTip = true;
+Vue.config.productionTip = true
+Vue.use(Router)
+
+const routes = [{
+  path: '/a',
+  component: A
+}, {
+  path: '/b',
+  component: B
+}];
+
+const router = new Router({
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app');
